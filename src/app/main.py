c016bc5ecf12dbe938c303b4a54c9760992b701b
Eduardo Araujo <eduardo.araujo@supero.com.br>
2021-12-15 19:11:38 -0400

    Adicionando API predict e payload

--- src/app/main.py
+++ src/app/main.py
@@ -9,6 +9,12 @@ import os
 from flask import Flask, request, jsonify
 from flask_basicauth import BasicAuth
 
+colunas = ['QuoteNumber', 'Field7', 'CoverageField6A', 'CoverageField6B', 'CoverageField8',
+            'CoverageField9', 'CoverageField11B', 'SalesField1A', 'SalesField1B', 'SalesField3',
+            'SalesField4', 'SalesField5', 'PersonalField1', 'PersonalField2', 'PersonalField9',
+            'PersonalField10A', 'PersonalField10B', 'PersonalField12', 'PropertyField34', 
+            'PropertyField35',  'PropertyField37']
+
 app = Flask(__name__)
 app.config["BASIC_AUTH_USERNAME"] = os.environ.get('BASIC_AUTH_USERNAME')
 app.config["BASIC_AUTH_PASSWORD"] = os.environ.get('BASIC_AUTH_PASSWORD')
@@ -18,7 +24,16 @@ basic_auth = BasicAuth(app)
 def load_model(file_name = 'model.pkl'):
     return pickle.load(open(file_name, "rb"))
 
-modelo = load_model(file_name = '../../models/model.pkl')
+#modelo = load_model(file_name = '../../models/model.pkl')
+
+@app.route('/predict', methods=['POST'])
+@basic_auth.required
+def predict():
+    # Pegar o JSON da requisição
+    dados = request.get_json()
+    payload = [dados[col] for col in colunas]
+
+    return payload
 
 # Rota padrão
 @app.route('/')
